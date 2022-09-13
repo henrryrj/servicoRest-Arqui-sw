4f7fbabde787427e112c15128b0404cf78757abc
henrryrj <henrryrocajoffre@gmail.com>
2022-09-13 15:01:01 -0400

    config cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors=require('cors');
+const corsOptions ={
+    origin:'*', 
+    credentials:true, //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
+
+
 const app = express();
 //require('./DBConecction');
 //settings
 app.set('port', process.env.PORT || 3000);
 // middlerwares
 app.use(express.json());
-
+app.use(cors(corsOptions));
 // Routes
 app.use('/apiRest/categoria/',require('./servicioRest/apiRestCategoria'));
 
