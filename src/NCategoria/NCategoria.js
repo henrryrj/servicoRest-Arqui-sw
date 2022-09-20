fc4c0feca7e28e4a3e6caacc1c093ed085d37609
henrryrj <henrryrocajoffre@gmail.com>
2022-09-20 15:25:57 -0400

    modificando variables

--- src/NCategoria/NCategoria.js
+++ src/NCategoria/NCategoria.js
@@ -1,17 +1,12 @@
 const DCategoria = require('../DCategoria/DCategoria');
 const dCategoria = new DCategoria();
-
-const addCategoria  = async (nombre) => await dCategoria.save(nombre);
-
-const editCategoria = async (id,nombre) => await dCategoria.update(id,nombre);
-
-const delCategoria  = async (id) => await dCategoria.delete(id);
-
-const getCategorias = async ()=> await dCategoria.getCategorias();
-
-module.exports ={
-    addCategoria,
-    editCategoria,
-    delCategoria,
-    getCategorias
+class NCategoria{
+    addCategoria  = async (nombre) => await dCategoria.save(nombre);
+    
+    editCategoria = async (id,nombre) => await dCategoria.update(id,nombre);
+    
+    delCategoria  = async (id) => await dCategoria.delete(id);
+    
+    getCategorias = async ()=> await dCategoria.getCategorias();
 }
+module.exports =NCategoria;
--- src/servicioRest/apiRestCategoria.js
+++ src/servicioRest/apiRestCategoria.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const root = express.Router();
-const {addCategoria,editCategoria,delCategoria,getCategorias} = require('../NCategoria/NCategoria');
+const NCategoria = require('../NCategoria/NCategoria');
 
+const root = express.Router();
+const nCategoria = new NCategoria();
 
 root.get('/getCategorias', async (_req,res)=>{
-    const categorias = await getCategorias();
+    const categorias = await nCategoria.getCategorias();
     res.status(200).json(categorias);
 });
 
 root.post('/save',async(req,res)=>{
     const {nombre} = req.body;
     if(nombre.length == 0) return res.status(404).json('undefined');
-    await addCategoria(nombre);
+    await nCategoria.addCategoria(nombre);
     res.status(200).json('ok');
 });
 
@@ -19,13 +20,13 @@ root.put('/edit/:id', async(req,res)=>{
     const {id} = req.params;
     const {nombre} = req.body;
     if(nombre.length == 0) return res.status(404).json('undefined');
-    await editCategoria(parseInt(id),nombre);
+    await nCategoria.editCategoria(parseInt(id),nombre);
     res.status(200).json('ok');
 });
 
 root.delete('/delete/:id', async(req,res)=>{
     const {id} = req.params;
-    await delCategoria(parseInt(id));
+    await nCategoria.delCategoria(parseInt(id));
     res.status(200).json('ok');
 });
 
